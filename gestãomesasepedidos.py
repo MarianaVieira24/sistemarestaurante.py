3b9a9b59c8e7b6273c18aa519704baf1bcd20a99
MarianaVieira24 <mariana202510521@grad.unifafire.edu.br>
2025-04-20 16:24:47 -0300

    gestãomesasepedidos.py

---
+++ gestãomesasepedidos.py
@@ -0,0 +1,2 @@
+mesas = {}  # Ex: {1: {'capacidade': 4, 'status': 'livre', 'clientes': [], 'pedidos': []}}
+pedidos = []  # Cada pedido: {'mesa': 1, 'itens': ['Prato1', 'Prato2'], 'status': 'recebido'}
